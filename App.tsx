36f470256a218ff5e73c7d521be0ae4f25d26d8f
Dilpesh786 <onlineerning33@gmail.com>
2025-12-27 01:45:11 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import SearchInterface from './components/SearchInterface';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <SearchInterface />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
